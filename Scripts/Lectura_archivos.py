81880040fdab69b503ed9f75a6b4496b5db84097
Jean <gseguierjc@gmail.com>
2022-11-29 16:43:06 -0500

    tu papi

--- Scripts/Lectura_archivos.py
+++ Scripts/Lectura_archivos.py
@@ -1,24 +1,17 @@
+import openpyxl
 import pandas as pd 
 import os
 
 def main():
-    df = leer_archivos()
-    df = agregar_filtros(df)
-
-    
-    #visualizar_datos(df)
-    exportar_datos(df)
-
-def leer_archivos():
-    print("Leyendo archivo")
-    
-
-    input_cols =  [0,2,3,4,8]
-
-    path = "C:/Users/rocha/OneDrive/Documents/Udemy/Python/Ejercicio/Input/"
+    pestanas = ["Cache","Host","Infra"]
     filename = input("Ingresar el nombre del archivo: ") + ".xlsx"
-    fullpath = os.path.join(path,filename)
+    write_excel(filename,pestanas)
 
+def leer_archivos(filename):
+    print("Leyendo archivo")
+    input_cols =  [0,2,3,4,8]
+    path = "C:/Proyectos/CoberturaMonitoreo/test-panda/Input"
+    fullpath = os.path.join(path,filename)
     df = pd.read_excel(fullpath,
                     sheet_name="Aplicaciones y tecnologías",
                     header=0,
@@ -26,42 +19,43 @@ def leer_archivos():
 
     return df
 
-def agregar_filtros(df):
+def agregar_filtros(df,cod_adpp,filtro):
 
     df = df[df["Ambiente"] == "Producción"]
     df = df.drop_duplicates(subset ="Equipo")
     print("Agregando filtros por comando")
-
-    cod_adpp = input("Ingresar codigo de aplicacion en filtrar: ")
     df = df[df["Código de aplicación"] == cod_adpp]
     
-    #Condition por pestaña Host
-    df = df[df["Equipo"].str.startswith('p')]
-    #Condition por pestaña Cache
-    df = df[df["Tecnología"].str.contains('Cache')]
+    print("dataframe")
+    print (df)
+
+    match filtro:
+        case "Cache":
+            print("switch cache")
+            return df[df["Tecnología"].str.contains(filtro,na=False)]
+        case "Host":
+            return df[df["Equipo"].str.startswith('p')]
+        # case "Infra":
+        #     return df
 
     return df
 
-def exportar_datos(df):
-    print("Exportando archivo proceso")
 
-    df.to_excel("C:/Users/rocha/OneDrive/Documents/Udemy/Python/Ejercicio/Output/Exportado.xlsx", 
-                index = False,
-                sheet_name= "Host")
-    df.to_excel("C:/Users/rocha/OneDrive/Documents/Udemy/Python/Ejercicio/Output/Exportado.xlsx", 
-                index = False,
-                sheet_name= "Cache")
-    
+
+def write_excel(filename,pestanas):
+    print("Escribiendo")
+    df = leer_archivos(filename)
+    cod_adpp = input("Ingresar codigo de aplicacion en filtrar: ")
+    path = "C:/Proyectos/CoberturaMonitoreo/test-panda/Output/Exportado.xlsx"
+    with pd.ExcelWriter(path) as writer:
+        for x in pestanas:
+            dataframe = agregar_filtros(df,cod_adpp,x) 
+            dataframe.to_excel(writer, sheet_name=x)
+        
+
+
 
 if __name__ == "__main__":
     main()
     input("\tPROCESO FINALIZADO. Presionar Enter para salir")
 
-
-#   def visualizar_datos(df):
-#   print("Visualizando los primeros 5 registros")
-#
-#   df_cols = df.columns
-#
-#   for col in df_cols:
-#       print(df[col].head(5))
